e4ca8486fdd6f789b13e2a273cd136689fbc124f
wooffyy <wahfiuddin3173@gmail.com>
2026-07-17 23:39:07 +0700

    feat(messageHandler): improve message handling and logging for replies and message chains

--- wa-gateway/src/whatsapp/messageHandler.js
+++ wa-gateway/src/whatsapp/messageHandler.js
@@ -1,15 +1,58 @@
+const { saveMessage, getReplyChain } = require('../ingestion/replyChain');
+const { handleWindowActivity } = require('../ingestion/windowManager');
+
 async function handleMessage(sock, m) {
     if (m.type !== 'notify') return;
 
     const msg = m.messages[0];
     if (!msg.message) return;
 
-    console.log(JSON.stringify(msg, null, 2));
+    const msgId = msg.key.id;
+    const chatId = msg.key.remoteJid;
+    const sender = msg.key.participant || msg.key.remoteJid.split('@')[0];
 
-    const quotedMessage = msg.message.extendedTextMessage?.contextInfo.quotedMessage;
+    const msgContent = msg.message.documentWithCaptionMessage?.message || msg.message;
 
-    if (quotedMessage){
-        console.log("quoted:", JSON.stringify(quotedMessage, null, 2));
+    const text = msgContent.conversation || 
+        msgContent.extendedTextMessage?.text || 
+        msgContent.documentMessage?.caption || 
+        msgContent.imageMessage?.caption || 
+        msgContent.videoMessage?.caption || 
+        msg.message.extendedTextMessage?.text ||
+        '';
+
+    const parentMsgId = msg.message.extendedTextMessage?.contextInfo?.stanzaId ||
+        msg.message.imageMessage?.contextInfo?.stanzaId ||
+        msg.message.videoMessage?.contextInfo?.stanzaId ||
+        msg.message.documentMessage?.contextInfo?.stanzaId ||
+        null;
+
+    const isReply = !!parentMsgId;
+
+    const msgData = {
+        id: msgId,
+        chatId, 
+        sender,
+        text,
+        parentMsgId: parentMsgId || null,
+        timestamp: Date.now()
+    };
+
+    await saveMessage(msgId, msgData);
+    console.log(`[Message saved] msgId: ${msgId} chatId: ${chatId} sender: ${sender} text: ${text}`);
+
+    await handleWindowActivity(chatId, isReply);
+    if (isReply) {
+        console.log(`[Message reply] msgId: ${msgId} chatId: ${chatId} sender: ${sender} text: ${text}`);
+        const chain = await getReplyChain(parentMsgId);
+
+        if (chain.length > 0) {
+            chain.forEach((prevMsg, index) => {
+            console.log(`[Message chain] ${index}. ${prevMsg.sender}: ${prevMsg.text}`);
+            }); 
+        } else {
+            console.log('Message Not Found in Redis')
+        }        
     }
 }
 
